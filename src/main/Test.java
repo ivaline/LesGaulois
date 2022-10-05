75265fc0c0a4bfa599e4105bd6ed58083876fce5
ivaline <alina.iva.4@gmail.com>
2022-10-05 14:03:00 +0200

    Nouveau projet

---
+++ src/main/Test.java
@@ -0,0 +1,13 @@
+package main;
+import pesonnages.Gaulois;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		asterix.parler("Bonjour");
+		
+
+	}
+
+}
---
+++ src/politique/Village.java
@@ -0,0 +1,14 @@
+package politique;
+
+public class Village {
+	private String nom;
+	public Village(String nom) {
+		super();
+		this.nom = nom;
+	}
+	public String getNom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}

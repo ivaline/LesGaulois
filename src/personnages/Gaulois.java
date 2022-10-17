7bbab06fa4b2685dfc2ede0b28733ca21158b2d2
ivaline <alina.iva.4@gmail.com>
2022-10-17 17:40:50 +0200

    TP 3 debut

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,32 +1,63 @@
 package personnages;
+import personnages.Romain;
+
 
 public class Gaulois {
 	private String nom;
-	private int force;
+	private int force; 
+	private int nbtrophees;
 	private int effetPotion = 1;
+	private Equipement[] trophees = new Equipement[100];
+	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 	}
+	
+	
 	public String getNom() {
 		return nom;
 	}
+	
+	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "« " + texte + "»");
 	}
+	
+	
+	//private String prendreParole() {
+		//return "Le gaulois " + nom + " : ";
+	//}
+	
 	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
-	}
+		return ("Le gaulois " + nom + " : ");
+		}
+
+	
+	
+	//public void frapper(Romain romain) {
+		//System.out.println(nom + " envoie un grand coup dans la mâchoire de "
+			//	+ romain.getNom());
+		//romain.recevoirCoup(force / 3);
+	//}
+	
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la mâchoire de "
-				+ romain.getNom());
-		romain.recevoirCoup(force / 3);
+		System.out.println(nom + " envoie un grand coup dans lamâchoire de " + romain.getNom());
+		Equipement trophees[] = romain.recevoirCoup((force / 3) * effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++, nb_trophees++) {
+			this.trophees[nb_trophees] = trophees[i];
+		}
+		return;
 	}
-	@Override
+
+
+	
 	public String toString() {
 		return "Gaulois [nom=" + nom + ", force=" + force
 				+ ", effetPotion=" + effetPotion + "]";
 	}
+	
+	
 	public static void main(String[] args) {
 	//TODO créer un main permettant de tester la classe Gaulois
 	}
--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,12 +1,14 @@
 package personnages;
 import personnages.Equipement;
 
+
 public class Romain {
 	
 	private String nom;
 	private int force;
 	private Equipement[] equipements;
 	private int nbEquipement = 0;
+	private String texte;
 	
 	public Romain(String nom, int force) {
 		assert (force>=0);
@@ -50,4 +52,74 @@ public class Romain {
 		}
 		
 	}
+	
+	public Equipement[] recevoirCoup(int forceCoup) {
+		Equipement[] equipementEjecte = null;
+		// précondition
+		assert force > 0;
+		int oldForce = force;
+		forceCoup = CalculResistanceEquipement(forceCoup);
+		force -= forceCoup;
+		// if (force > 0) {
+		// parler("Aïe");
+		// } else {
+		// equipementEjecte = ejecterEquipement();
+		// parler("J'abandonne...");
+		// }
+		switch (force) {
+		case 0:
+			parler("Aïe");
+		default:
+			equipementEjecte = ejecterEquipement();
+			parler("J'abandonne...");
+			break;
+			}
+		// post condition la force à diminuer
+		assert force < oldForce;
+		return equipementEjecte;
+		}
+	
+	private int CalculResistanceEquipement(int forceCoup) {
+		texte = "Ma force est de " + this.force + ", et la force du coup est de " + forceCoup;
+		int resistanceEquipement = 0;
+		if (!(nbEquipement == 0)) {
+		texte += "\nMais heureusement, grace à mon équipement sa force est diminué de ";
+			for (int i = 0; i < nbEquipement;) {
+				if ((equipements[i] != null &&
+					equipements[i].equals(Equipement.BOUCLIER)) == true) {
+						resistanceEquipement += 8;
+				} 
+				else {
+					System.out.println("Equipement casque");
+					resistanceEquipement += 5;
+				}
+				i++;
+			}
+			texte =+ resistanceEquipement + "!";
+		}
+		parler(texte);
+		forceCoup -= resistanceEquipement;
+		return forceCoup;
+		}
+	
+	private Equipement[] ejecterEquipement() {
+		Equipement[] equipementEjecte = new Equipement[nbEquipement];
+		System.out.println("L'équipement de " + nom.toString() + "s'envole sous la force du coup.");
+		//TODO
+		int nbEquipementEjecte = 0;
+		for (int i = 0; i < nbEquipement; i++) {
+			if (equipements[i] == null) {
+				continue;
+			} else {
+				equipementEjecte[nbEquipementEjecte] = equipements[i];
+				nbEquipementEjecte++;
+				equipements[i] = null;
+			}
+		}
+		return equipementEjecte;
+			}
+
+		}
+
+
 }
